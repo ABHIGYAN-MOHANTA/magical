5425179cf1e8cf57fb6a5e70d9e5ae46219ddbd0
abhigyan-mohanta <underthunder02@gmail.com>
2025-12-24 20:22:28 +0530

    build webgl

---
+++ magical/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "HostageDown-Magical-0.1.0";
+const contentToCache = [
+    "Build/magical.loader.js",
+    "Build/magical.framework.js.unityweb",
+    "Build/magical.data.unityweb",
+    "Build/magical.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
